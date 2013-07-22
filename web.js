821555bff28f9333aa0f1f8f9dd331a0812f0f5a
alexandrosgiamas <agiamas@gmail.com>
2013-07-22 12:16:00 +0300

    modified and styled index.html, added my custom marketing logos etc

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var fs = require('fs');
 var express = require('express');
 
 var app = express.createServer(express.logger());
-
+app.use(express.static(__dirname + '/public'));
 app.get('/', function(request, response) {
 	var indexContents = fs.readFileSync('index.html').toString();
 	response.send(indexContents);
@@ -11,5 +11,5 @@ app.get('/', function(request, response) {
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-  console.log("Listening on " + port);
+  console.log("Listening on " + port + " for dirname " + __dirname);
 });
